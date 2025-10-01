d1eba76aede31b16baddfcc65ddf7959c4f2540c
ToPhile7 <89874800+ToPhile7@users.noreply.github.com>
2025-10-01 09:59:23 +0200

    Updated chains.tsx

--- app/(tabs)/chains.tsx
+++ app/(tabs)/chains.tsx
@@ -221,7 +221,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
-    backgroundColor: 'transparent',
+    backgroundColor: '#000',
   },
   selectAllText: {
     fontSize: 18,
