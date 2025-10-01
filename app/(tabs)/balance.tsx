195ffd264ed5cc29a1261f2add08fc0dd48160fd
ToPhile7 <89874800+ToPhile7@users.noreply.github.com>
2025-10-01 08:42:49 +0200

    Updated balance.tsx

--- app/(tabs)/balance.tsx
+++ app/(tabs)/balance.tsx
@@ -33,7 +33,7 @@ export default function BalanceScreen() {
   const screenWidth = Dimensions.get('window').width;
   const isDesktop = screenWidth > 768;
   const backgroundSource = isDesktop 
-    ? require('../../assets/images/backgrounddesktop.png')
+    ? require('../../assets/images/background.png')
     : require('../../assets/images/background.png');
 
   React.useEffect(() => {
